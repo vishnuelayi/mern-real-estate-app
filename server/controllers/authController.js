a398248a029ea2e07bd080ba768cee58ade51549
Vishnu Elayi <vishnuelayi@gmail.com>
2024-03-16 00:19:53 +0530

    login controller modified into make a post request with either email or username

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signupController = asyncHandler(async (req, res) => {
     const { username, name, email, password, phone, address, city, state, zip, country } = req.body;
@@ -27,8 +28,13 @@ export const signupController = asyncHandler(async (req, res) => {
 
 
 export const loginController = asyncHandler(async (req, res) => {
-    const { username, password } = req.body;
-    const isValidUser = await User.findOne({ username });
+    const { usernameOrEmail, password } = req.body;
+    const isValidUser = await User.findOne({
+        $or: [
+            { username: usernameOrEmail },
+            { email: usernameOrEmail }
+        ]
+    });
     if(!isValidUser) {
         res.status(401);
         throw new Error("Invalid Credentials");
@@ -38,8 +44,13 @@ export const loginController = asyncHandler(async (req, res) => {
         res.status(401);
         throw new Error("Invalid Credentials");
     }
-    res.status(200).json({
+    const token = jwt.sign({ id: isValidUser._id }, process.env.JWT_SECRET);
+    res.cookie("access_token", token, {
+        httpOnly:true,
+        expires: new Date(Date.now() + (3 * 60 * 60 * 1000)) // 3 hours in milliseconds
+    }).status(200).json({
         message: "User logged in successfully",
         user: isValidUser,
     });
+    
 })
