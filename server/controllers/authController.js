7d1c1f11377b068f0217e5e5fb0c4a6b52157873
Vishnu Elayi <vishnuelayi@gmail.com>
2024-03-17 14:14:23 +0530

    implemented a controller for  google authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,48 +4,80 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const signupController = asyncHandler(async (req, res) => {
-    const { username, name, email, password, phone, address, city, state, zip, country } = req.body;
-    const hashedPassword = bcrypt.hashSync(password, 10);
+  const {
+    username,
+    name,
+    email,
+    password,
+    phone,
+    address,
+    city,
+    state,
+    zip,
+    country,
+  } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
 
-    try {
-        const user = await User.create({
-            username,
-            name,
-            email,
-            password:hashedPassword,
-            phone,
-            address,
-            city,
-            state,
-            zip,
-            country,
-        });
-        res.status(201).json(user);
-    } catch (error) {
-        res.status(500).json({ message: "Error creating user", error: error.message });
-    }
+  try {
+    const user = await User.create({
+      username,
+      name,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating user", error: error.message });
+  }
 });
 
-
 export const loginController = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    console.log(email, password);
-    
-    const isValidUser = await User.findOne({ email });
-    console.log(isValidUser);
-    if(!isValidUser) {
-        res.status(401);
-        throw new Error("Invalid Credentials");
-    }
-    const isPasswordCorrect = bcrypt.compareSync(password, isValidUser.password);
-    if(!isPasswordCorrect) {
-        res.status(401);
-        throw new Error("Invalid Credentials");
-    }
-    const token = jwt.sign({ id: isValidUser._id }, process.env.JWT_SECRET);
-    res.cookie("access_token", token, {
-        httpOnly:true,
-        expires: new Date(Date.now() + (3 * 60 * 60 * 1000)) // 3 hours in milliseconds
-    }).status(200).json(isValidUser);
-    
-})
+  const { email, password } = req.body;
+  console.log(email, password);
+
+  const isValidUser = await User.findOne({ email });
+  console.log(isValidUser);
+  if (!isValidUser) {
+    res.status(401);
+    throw new Error("Invalid Credentials");
+  }
+  const isPasswordCorrect = bcrypt.compareSync(password, isValidUser.password);
+  if (!isPasswordCorrect) {
+    res.status(401);
+    throw new Error("Invalid Credentials");
+  }
+  const token = jwt.sign({ id: isValidUser._id }, process.env.JWT_SECRET);
+  res
+    .cookie("access_token", token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 3 * 60 * 60 * 1000), // 3 hours in milliseconds
+    })
+    .status(200)
+    .json(isValidUser);
+});
+
+export const googleAuth = asyncHandler(async (req, res) => {
+  const { email, name } = req.body;
+
+  const user = User.findOne({ email });
+  if (user) {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 3 * 60 * 60 * 1000), // 3 hours in milliseconds
+      })
+      .status(200)
+      .json(user);
+  } else {
+    const user = await User.create({
+      name,
+      email,
+      password: "",
+      username: name?.toLowerCase().replace(/\s/g, ""),
+    });
+    res.status(201).json(user);
+  }
+});
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { loginController, signupController} from "../controllers/authController.js";
+import { googleAuth, loginController, signupController} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signupController);
 router.post("/login", loginController);
+router.post("/google", googleAuth)
 
 export default router;
