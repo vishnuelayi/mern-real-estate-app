e0b27925969c2826b1c9094f53baa596ac46bc44
Vishnu Elayi <vishnuelayi@gmail.com>
2024-03-18 21:12:10 +0530

    implemented the updateUser controller in user route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {demoRoute} from "../controllers/userController.js";
+import { updateUser } from "../controllers/userController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", demoRoute
-);
+router.post("/",authMiddleware ,updateUser);
+
 
 export default router;
