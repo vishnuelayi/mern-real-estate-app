69b8a4a3f412bdc7d2adfdeda2c53c7769cb0c68
Vishnu Elayi <vishnuelayi@gmail.com>
2024-03-19 13:47:15 +0530

    image upload configuration has been completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { MdEditSquare } from "react-icons/md";
 import { useDispatch } from "react-redux";
 import { updateUser } from "../features/user/userService";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+;
 
 const validationSchema = Yup.object({
   username: Yup.string().required("Username is required"),
@@ -12,8 +21,50 @@ const validationSchema = Yup.object({
 });
 
 const Profile = () => {
-  
   const dispatch = useDispatch();
+
+  const fileRef = useRef();
+  const [file, setFile] = useState(undefined);
+  const [filePercentage, setFilePercentage] = useState(0);
+  const [fileURL, setFileURL] = useState(undefined);
+  const [uploadError, setUploadError] = useState(false);
+
+  const fileUR = "fahgaogjohgojgojgojg";
+
+  const [formData, setFormData] = useState({})
+ console.log(formData);
+
+  
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+
+  //firebase image upload configuration
+  const handleFileUpload = async (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    uploadBytesResumable(storageRef, file)
+      .then((snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePercentage(progress);
+        if (progress === 100) {
+          getDownloadURL(storageRef).then((url) => {
+            setFileURL(url);
+          });
+        }
+      })
+      .catch((error) => {
+        setUploadError(true);
+        console.log(error);
+      });
+  };
+
+
 
   const singnedInUser = JSON.parse(localStorage.getItem("user"));
 
@@ -25,23 +76,33 @@ const Profile = () => {
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      dispatch(updateUser(JSON.stringify(values)));
+      setFormData(JSON.stringify(values));
+      // dispatch(updateUser(JSON.stringify(values)));
     },
   });
 
-  const handleLogout  = () => {
+  const handleLogout = () => {
     localStorage.clear();
     window.location.href = "/login";
-  }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="font-semibold text-3xl text-center my-7">Profile</h1>
       <form className="flex flex-col gap-3" onSubmit={formik.handleSubmit}>
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+
         <img
           src={singnedInUser?.image}
           alt="profile image"
-          className="rounded-full h-24 w-24 self-center object-cover cursor-pointer mt-2"
+          className="rounded-full h-24 w-24 self-center object-cover cursor-pointer mt-2 mb-2"
+          onClick={() => fileRef.current.click()}
         />
 
         <div className="relative">
@@ -90,7 +151,6 @@ const Profile = () => {
             value={formik.values.email}
             disabled
           />
-          
         </div>
 
         <button
@@ -100,7 +160,11 @@ const Profile = () => {
           Update
         </button>
 
-        <button className="text-white bg-red-600 rounded-lg p-3 uppercase disabled:opacity-80 hover:opacity-95" type="button" onClick={handleLogout}>
+        <button
+          className="text-white bg-red-600 rounded-lg p-3 uppercase disabled:opacity-80 hover:opacity-95"
+          type="button"
+          onClick={handleLogout}
+        >
           Logout
         </button>
       </form>
