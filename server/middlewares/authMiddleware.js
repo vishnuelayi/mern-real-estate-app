5b05c85a3bd4ddc731bf7ba181c3b74ba4200edf
Vishnu Elayi <vishnuelayi@gmail.com>
2024-03-22 13:07:48 +0530

    implemented the single property fetching controller and route

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -16,7 +16,6 @@ export const authMiddleware = asyncHandler(async (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         const user = await User.findById(decoded?.id);
-        console.log(user);
 
         req.user = user;
 
--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { listOneItem } from "../controllers/ListingController.js";
+import { getItemsByUser, getSingleItem, listOneItem } from "../controllers/ListingController.js";
 
 const router = express.Router();
 
 
 router.post("/add", authMiddleware,listOneItem);
+router.get("/:id",getSingleItem)
+router.get("/mylistings", authMiddleware,getItemsByUser)
+
 
 
 export default router;
