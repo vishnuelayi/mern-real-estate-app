68ab9f1aa0c94f31966c46617c34cdfae4638e08
Vishnu Elayi <vishnuelayi@gmail.com>
2024-03-27 01:07:58 +0530

    reverted the listing route

--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -1,19 +1,18 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import {
-  deleteItem,
-  getItemsByUser,
-  getItemsOnQuery,
-  getSingleItem,
-  listOneItem,
-} from "../controllers/ListingController.js";
+import { deleteItem, getItemsByUser, getItemsOnQuery, getSingleItem, listOneItem } from "../controllers/ListingController.js";
 
 const router = express.Router();
 
-router.post("/", authMiddleware, listOneItem);
-router.get("/", getItemsOnQuery);
-router.get("/my-listings", authMiddleware, getItemsByUser);
-router.get("/:id", getSingleItem);
-router.delete("/:id", authMiddleware, deleteItem);
+
+router.post("/add", authMiddleware,listOneItem);
+
+router.get("/mylistings", authMiddleware,getItemsByUser)
+router.delete("/delete/:id",authMiddleware,deleteItem)
+router.get("/get",getItemsOnQuery)
+router.get("/:id",getSingleItem)
+
+
+
 
 export default router;
